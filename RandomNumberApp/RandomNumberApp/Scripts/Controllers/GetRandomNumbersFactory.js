473807cc040ba705879dc9134ba78c2e8820deae
ahmadal <ahmadal@datacom.co.nz>
2016-07-10 00:24:14 +1200

    some code cleanup

--- RandomNumberApp/RandomNumberApp/Scripts/Controllers/GetRandomNumbersFactory.js
+++ RandomNumberApp/RandomNumberApp/Scripts/Controllers/GetRandomNumbersFactory.js
@@ -3,8 +3,8 @@
         var deferredObject = $q.defer();
         $http.get("https://qrng.anu.edu.au/API/jsonI.php?length=" + length + "&type=uint8")
         .then(function sucessCallback(response) {
-                console.log("success");
-                deferredObject.resolve(response.data);
+            console.log("success");
+            deferredObject.resolve(response.data);
 
         }, function errorCallback(error) {
             console.log("error http get in scope.getRandomNumbers method: " + error);
@@ -12,6 +12,5 @@
 
         return deferredObject.promise;
     }
-
 }
 GetRandomNumbersFactory.$inject = ['$http', '$q'];
